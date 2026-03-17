32f729640554e3fc87a29eadafdbd81fd02a5e37
karnandohuang <karnandohuang@outlook.com>
2026-03-17 23:30:27 +0700

    add useImperativeHandle

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,6 +1,18 @@
+import {useImperativeHandle, useRef} from "react";
+
 function ResultModal ({ ref, result, targetTime }) {
+    useImperativeHandle(ref, () => {
+        return {
+            open: () => {
+                dialogRef.current.showModal()
+            }
+        }
+    })
+
+    const dialogRef = useRef();
+
     return (
-        <dialog ref={ref} className="result-modal">
+        <dialog ref={dialogRef} className="result-modal">
             <h2>You {result}!</h2>
             <p>The target time was <strong>{targetTime} seconds.</strong></p>
             <p>You stopped the timer with <strong>x seconds left.</strong></p>
