35735c3a2680bff323a6fdb77549a8947b8f8e2b
karnandohuang <karnandohuang@outlook.com>
2026-03-17 22:29:54 +0700

    added Player.jsx and TimerChallenge.jsx

---
+++ src/components/Player.jsx
@@ -0,0 +1,21 @@
+import {useRef, useState} from "react";
+
+export default function Player() {
+    const [enteredPlayerName, setEnteredPlayerName] = useState(null)
+
+    const playerName = useRef();
+    const handleCLick = () => {
+        setEnteredPlayerName(playerName.current.value)
+        playerName.current.value = ''
+    }
+    
+  return (
+    <section id="player">
+      <h2>Welcome { enteredPlayerName ?? 'unknown entity'}</h2>
+      <p>
+        <input ref={playerName} type="text" />
+        <button onClick={handleCLick}>Set Name</button>
+      </p>
+    </section>
+  );
+}
